85857e02ad1289f987dc9f5fde85b2151679dd28
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-11-22 22:40:28 -0300

    fix: adiciona random_string no terraform para ssh key

--- terraform/virtual-machine.tf
+++ terraform/virtual-machine.tf
@@ -1,6 +1,14 @@
+resource "random_string" "this" {
+  length      = 5
+  special     = false
+  lower       = true
+  min_numeric = 2
+  min_lower   = 3
+}
+
 resource "mgc_ssh_keys" "this" {
   provider = mgc.sudeste
-  name     = "mgc_vm_ssh_key"
+  name     = "mgc_ssh_key_${random_string.this.id}"
   key      = file("../mgc_ssh_key.pub")
 }
 
